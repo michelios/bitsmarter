1a18aae416c59198c5435d01e97995dbb799b128
root <root@gallifrey.(none)>
2013-07-11 18:14:03 +0000

    return to the previous heroku machine

--- web.js
+++ web.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var fs = require('fs');
+/*var buffer = require('buf');*/
+var path = "./index.html";
+var output = "";
+
+if (fs.existsSync(path)){
+
+	output = fs.readFileSync(path, 'utf8');
+	
+}else{
+
+	output = "error:index not found";
+}
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
